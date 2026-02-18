534a6b89ef0f72814b70ae69e3cf9f63a119af4e
Nafiul-Islam-Eshan <nafiulislameshan307@gmail.com>
2026-02-18 19:29:24 +0600

    Array of duas

---
+++ script.js
@@ -0,0 +1,29 @@
+const duas = [
+    {
+        arabic: "رَبَّنَا آتِنَا مِنْ لَدُنْكَ رَحْمَةً وَهَيِّئْ لَنَا مِنْ أَمْرِنَا رَشَدً",
+        english: "Our Lord, grant us from Yourself mercy and prepare for us from our affair, right guidance.",
+    },
+    {
+        arabic:  "لَا إِلَهَ إِلَّا اللهُ وَحْدَهُ لَا شَرِيكَ لَهُ، لَهُ الْمُلْكُ وَلَهُ الْحَمْدَ، وَهُوَ عَلَى كُلِّ شَيْءٍ قَدِيرٌ. سُبْحَانَ اللهِ، وَالْحَمْدُ للهِ، ولَا إِلَهَ إِلَّا اللهُ، وَاللهُ أَكْبَرُ، وَلَا حَوْلَ وَلَا قُوَّةَ إِلَّا بِاللهِ العَلِيِّ الْعَظيِمِ، ربِّ اغْفِرلِي",
+        english: "There is none worth of worship but Allah alone, Who has no partner, His is the dominion and to Him belongs all praise, and He is able to do all things. Glory is to Allah. Praise is to Allah. There is none worth of worship but Allah. Allah is the Most Great. There is no might and no power except by Allah’s leave, the Exalted, the Mighty. My Lord, forgive me."
+    },
+    {
+        arabic: "ذَهَبَ الظَّمَأُ وَابْتَلَّتِ الْعُرُوقُ وَثَبَتَ الأَجْرُ إِنْ شَاءَ اللَّهُ",
+        english: "The thirst has gone, the veins have been moistened, and the reward is confirmed if Allah wills.",
+    },
+    {
+        arabic: "رَبَّنَا لَا تُزِغْ قُلُوبَنَا بَعْدَ إِذْ هَدَيْتَنَا وَهَبْ لَنَا مِن لَّدُنكَ رَحْمَةً ۚ إِنَّكَ أَنتَ الْوَهَّابُ",
+        english: "Our Lord, do not let our hearts deviate after You have guided us, and bestow upon us mercy from Yourself. Indeed, You are the Bestower.",
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
